04f687ad1812a380a184031fa3b7b5c961747cf4
superstarlol999 <34463182+superstarlol999@users.noreply.github.com>
2021-12-02 20:47:18 +0700

    fix_bugs_and_decode

--- GeneratorCustom.py
+++ GeneratorCustom.py
@@ -1,23 +1,21 @@
 import numpy as np
 from tensorflow.keras.utils import Sequence
 import cv2
-
-class DataLoader(Sequence):
-    def __init__(self, all_filenames, input_size = (256, 256), batch_size = 32, shuffle = True, seed = 123, encode: dict = None) -> None:
-        super(DataLoader, self).__init__()
+import tensorflow as tf 
+class DataGenerator(Sequence):
+    def __init__(self, all_filenames, input_size = (256, 256), batch_size = 32, shuffle = True, seed = 123, encode: dict = None, color_mode = 'hsv') -> None:
+        super(DataGenerator, self).__init__()
         assert encode != None,  'Not empty !'
         self.all_filenames = all_filenames
         self.input_size = input_size
         self.batch_size = batch_size
         self.shuffle = shuffle
+        self.color_mode = color_mode
         np.random.seed(seed)
         self.on_epoch_end()
     def processing(self, mask):
-        d  = [] 
-        for i in mask.reshape(-1,3):
-            d.append(self.encode[tuple(i)])
-        return np.array(d).reshape(mask.shape[0], mask.shape[1], mask.shape[2], 1)
-
+        d = list(map(lambda x: self.encode[tuple(x)], mask.reshape(-1,3)))
+        return np.array(d).reshape(*self.input_size, 1)
     def __len__(self):
         return int(np.floor(len(self.all_train) / self.batch_size))
     def __getitem__(self, index):
@@ -34,14 +32,23 @@ class DataLoader(Sequence):
         X = np.empty(shape=(batch, *self.input_size, 3))
         Y = np.empty(shape=(batch, *self.input_size, 1))
         for i, (fn, label_fn) in enumerate(all_filenames_temp):
+            # img
             img = cv2.imread(fn)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-            img = cv2.resize(img, self.input_size)
+            if self.color_mode == 'hsv':
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+            elif self.color_mode == 'rgb':
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = tf.image.resize(img, (256,256), method = 'nearest')
+            img = tf.cast(img, tf.float32)
             img /= 255.
-            mask = cv2.imread(label_fn)
-            mask = cv2.resize(mask, self.input_size)
+            
+            #mask
+            mask = cv2.imread(label_fn, 0)
+            mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
+            mask = tf.image.resize(mask, (256,256), method= 'nearest')
+            mask = np.array(mask)
             mask = self.processing(mask)
+            mask = tf.cast(mask, tf.float32)
             X[i,] = img
             Y[i,] = mask
         return X, Y
-
--- data.py
+++ data.py
@@ -1,6 +1,10 @@
 import pickle
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from GeneratorCustom import DataGenerator
+import numpy as np
+import sklearn
+import cv2
+import tensorflow as tf
 
 def encode_label(mask):
     # input (batch, rows, cols, channels)
@@ -9,22 +13,24 @@ def encode_label(mask):
         label.append(tuple(i))
     label = set(label)
     encoder = dict((j,i) for i,j in enumerate(label)) # key is tuple 
-    with open('label.obj', 'w', encoding= 'utf-8') as f:
-        pickle.dump(encoder, f)
+    with open('label.pickle', 'wb') as handel:
+        pickle.dump(encoder, handel, protocol= pickle.HIGHEST_PROTOCOL)
     return encoder
-
-def DataLoader(all_train_filename, all_valid_filename, train_folder, mask_folder = 'label', input_size = (256,256), batch_size = 32, shuffle = True, seed = 123) -> None:
-    # Encode
-    mask_datagen = ImageDataGenerator().flow_from_directory(
-        train_folder,
-        classes= [mask_folder],
-        color_mode= 'rgb',
-        class_mode= None,
-        shuffle= True,
-        seed = 123,
-        batch_size= 8
-    )
-    encode = encode_label(mask_datagen[0])
+def decode_label(predict, decode):
+    predict = tf.squeeze(predict, axis = 0)
+    predict = np.argmax(predict, axis = 2) 
+    d = list(map( lambda x: decode[int(x)], predict.reshape(-1,1)))
+    img =  np.array(d).reshape(predict.shape[0], predict.shape[1], 3)
+    b,g,r = cv2.split(img)
+    return cv2.merge((r,g,b))
+def DataLoader(all_train_filename, all_mask,  all_valid_filename = None, input_size = (256,256), batch_size = 4, shuffle = True, seed = 123, color_mode = 'hsv') -> None:
+    mask_folder = sklearn.utils.shuffle(all_mask, random_state = 47)[:8]
+    mask = [tf.image.resize(cv2.cvtColor(cv2.imread(img), cv2.COLOR_BGR2RGB), input_size, method = 'nearest') for img in mask_folder ]
+    mask = np.array(mask)
+    encode = encode_label(mask)
     train = DataGenerator(all_train_filename, input_size, batch_size , shuffle, seed, encode)
-    valid = DataGenerator(all_valid_filename, input_size, batch_size, shuffle, seed, encode)
-    return train, valid
+    if all_valid_filename == None: 
+        return train
+    else:
+        valid = DataGenerator(all_valid_filename, input_size, batch_size, shuffle, seed, encode)
+        return train, valid
