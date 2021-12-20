aceb7ee9860a74412fc153604aecef0b2d029134
superstarlol999 <34463182+superstarlol999@users.noreply.github.com>
2021-12-20 18:39:30 +0700

    fix bugs

--- predict_func.py
+++ predict_func.py
@@ -1,18 +1,22 @@
 import cv2
 import tensorflow as tf
-import data
+from data import *
 import numpy as np
 def predict(model, image_test, label, color_mode, size):
     image = cv2.imread(image_test)
     if color_mode == 'hsv':
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    else:
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = tf.image.resize(image, size, method= 'nearest')
-    image = tf.cast(image, tf.float32) 
-    image_norm = image / 255.
+        image_cvt = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    elif color_mode == 'rgb':
+        image_cvt = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    elif color_mode == 'gray':
+        image_cvt = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image_cvt = tf.expand_dims(image_cvt, axis = 2)
+    image_cvt = tf.image.resize(image_cvt, size, method= 'nearest')
+    image_cvt = tf.cast(image_cvt, tf.float32) 
+    image_norm = image_cvt / 255.
     image_norm = tf.expand_dims(image_norm, axis= 0)
     new_image = model(image_norm)
-    image_decode = data.decode_label(new_image, label)
-    predict_img = image * 0.5 + image_decode * 0.5
-    return np.floor(predict_img)[0].astype('int'), new_image
+    image_argmax = np.argmax(tf.squeeze(new_image, axis = 0), axis = 2)
+    image_decode = decode_label(image_argmax, label)
+    predict_img = tf.cast(tf.image.resize(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), size, method = 'nearest'), tf.float32) * 0.7 + image_decode * 0.3
+    return np.floor(predict_img).astype('int'), new_image
